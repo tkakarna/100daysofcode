c48c1cff1481f2b4b4d44ede73bc62d414c4be58
tkakarna <38823410+tkakarna@users.noreply.github.com>
2018-04-30 21:08:08 -0400

    Fixed the code, should be able to give char as input

--- quadratic.cpp
+++ quadratic.cpp
@@ -9,33 +9,36 @@ int main(){
 
   double root1, root2, a, b, c, root;
 
+  int temp1,temp2,temp3;
+
   cout << "Enter the coefficients, a, b, c \n";
   cin >> a >> b >> c;
- 
-  if ((a==0) & (b==0) & (c==0)) {
-     cout << "Undefined coefficients \n";
-   } 
-else {
-  root = sqrt(b*b - 4.0 * a * c);
-  if (!(root > 0)) {
-    cout << "The root value is negative, so the coefficients are not valid \n Please re-enter the coefficients \n";
-    cin >> a >> b >> c;
-    if ((a==0) & (b==0) & (c==0)) {
+
+  temp1=a;
+  temp2=b;
+  temp3=c;
+
+  if (a==0) {
+     cout << "a is zero or undefined \n";
+  }
+  else {
+    if ((temp1==0) | (temp2==0) | (temp3==0)) {
       cout << "Undefined coefficients \n";
     }
-    root = sqrt(b*b - 4.0 * a * c);
-    cout << "root = sqrt() " << root << "\n";
-    root1 = ((0.5*(root - b))/ a);
-    root2 = ((-0.5*(root + b))/ a);
-    cout << "The solutions are " << root1 << " and " << root2 << "\n";
-    
+    else
+    {
+      root = sqrt(b*b - 4.0 * a * c);
+      if (!(root > 0)) {
+        cout << "The root value is negative, so the coefficients are not valid \n";
+      }
+      else
+        {
+          cout << "root = sqrt() - " << root << "\n";
+          root1 = ((0.5*(root - b))/ a);
+          root2 = ((-0.5*(root + b))/ a);
+          cout << "The solutions are " << root1 << " and " << root2 << "\n";
+        }
+    }
   }
-  else { 
-    cout << "root = sqrt() - " << root << "\n";
-    root1 = ((0.5*(root - b))/ a);
-    root2 = ((-0.5*(root + b))/ a);
-    cout << "The solutions are " << root1 << " and " << root2 << "\n";
-  }
- }
-return(0);
+  return(0);
 }
